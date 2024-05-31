e2492c365cec9366a00e7e100722ef09851ac21a
FalseHope <haichensurfs@gmail.com>
2024-05-31 23:56:34 +0800

    sample app with thread creation login and register

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,10 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://localhost:3000"
 };
-const db = require("./app/models");
-db.sequelize.sync();
+//const db = require("./app/models");
+//db.sequelize.sync();
 app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
@@ -19,11 +19,89 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Hello to here" });
 });
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
+});
+
+//👇🏻 holds all the existing users
+const users = [];
+//👇🏻 generates a random string as ID
+const generateID = () => Math.random().toString(36).substring(2, 10);
+
+app.post("/api/register", async (req, res) => {
+    const { email, password, username } = req.body;
+    //👇🏻 holds the ID
+    const id = generateID();
+    //👇🏻 logs all the user's credentials to the console.
+    const result = users.filter(
+      (user) => user.email === email && user.password === password
+    );
+    //👇🏻 if true
+    if (result.length === 0) {
+        const newUser = { id, email, password, username };
+        //👇🏻 adds the user to the database (array)
+        users.push(newUser);
+        //👇🏻 returns a success message
+        return res.json({
+            message: "Account created successfully!",
+        });
+    }
+    //👇🏻 if there is an existing user
+    res.json({
+        error_message: "User already exists",
+    });
+});
+
+app.post("/api/login", (req, res) => {
+  const { email, password } = req.body;
+  //👇🏻 checks if the user exists
+  let result = users.filter(
+      (user) => user.email === email && user.password === password
+  );
+  //👇🏻 if the user doesn't exist
+  if (result.length !== 1) {
+      return res.json({
+          error_message: "Incorrect credentials",
+      });
+  }
+  //👇🏻 Returns the id if successfuly logged in
+  res.json({
+      message: "Login successfully",
+      id: result[0].id,
+  });
+});
+
+
+//👇🏻 holds all the posts created
+const threadList = [];
+
+app.post("/api/create/thread", async (req, res) => {
+const { thread, userId } = req.body;
+const threadId = generateID();
+
+    //👇🏻 add post details to the array
+    threadList.unshift({
+        id: threadId,
+        title: thread,
+        userId,
+        replies: [],
+        likes: [],
+    });
+
+    //👇🏻 Returns a response containing the posts
+    res.json({
+        message: "Thread created successfully!",
+        threads: threadList,
+    });
+});
+
+app.get("/api/all/threads", (req, res) => {
+  res.json({
+      threads: threadList,
+  });
 });
--- frontend/react-crud/src/index.js
+++ frontend/react-crud/src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,9 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
-  <BrowserRouter>
     <App />
-  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
