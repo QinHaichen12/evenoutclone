e890ccb84b69cb49aacdbe93f03b8be2dc38605a
QinHaichen12 <haichensurfs@gmail.com>
2024-07-02 10:27:26 +0800

    supabase

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -34,10 +34,10 @@ export async function signup(formData: FormData) {
     email: formData.get('email') as string,
     password: formData.get('password') as string,
   }
-
   const { error } = await supabase.auth.signUp(data)
 
   if (error) {
+
     redirect('/error')
   }
 
